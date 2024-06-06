e7c959b44cb0c7a3ae03fd0bd325cf11a3e0a0c5
emerson <emerson8096@gmail.com>
2024-06-06 16:20:26 -0300

    add details

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import { Container, Content } from "./styles";
+import { useParams, useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
+import { useAuth } from "../../hooks/auth";
+
+import { Tags } from "../../components/Tags";
+import { ButtonText } from "../../components/ButtonText";
+import { Header } from "../../components/Header";
+
+import { FaClock } from "react-icons/fa";
+import { FiArrowLeft } from "react-icons/fi";
+import StarImg from "../../assets/star.svg";
+import StarImgDisable from "../../assets/stardisable.svg";
+
+export function Details() {
+  const { user } = useAuth();
+
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const avatarURL = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <img
+          key={i}
+          src={i <= data.rating ? StarImg : StarImgDisable}
+          alt="Avaliação do filme"
+        />
+      );
+    }
+    return stars;
+  };
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/movie_notes/${params.id}`);
+      navigate(-1);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/movie_notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
+
+  return (
+    <Container>
+      <Header />
+
+      {data && (
+        <main>
+          <Content>
+            <header>
+              <FiArrowLeft />
+              <ButtonText title="Voltar" onClick={handleBack} />
+            </header>
+
+            <div className="title">
+              <h1>{data.title}</h1>
+              <div>{renderStars(data.rating)}</div>
+              <button className="button" onClick={handleRemove}>Excluir filme</button>
+            </div>
+
+            <div className="author">
+              <img src={avatarURL} alt="{data.name}" />
+
+              <p>Por {user.name}</p>
+
+              <FaClock />
+
+              <p>{user.updated_at}</p>
+            </div>
+
+            {data.tags && <Tags data={data.tags} />}
+
+            <p id="p">{data.description}</p>
+          </Content>
+        </main>
+      )}
+    </Container>
+  );
+}
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,122 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas: 
+  "header"
+  "content";
+
+  > main {
+    grid-area: content;
+    overflow-y: scroll;
+    padding: 64px 0;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 10px;
+
+      margin-bottom: 36px;
+
+      color: ${({ theme }) => theme.COLORS.PINK};
+
+      a {
+        font-size: 16px;
+        color: ${({ theme }) => theme.COLORS.PINK};
+        gap: 10px;
+      }
+    }
+    
+    .title {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 20px;
+
+      > div {
+        display: flex;
+        gap: 5px;
+      }
+
+      img {
+        width: 20px;
+        height: 20px;
+      }
+
+      button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin-left: auto;
+
+        height: 56px;
+        width: 150px;
+
+        border: 0;
+        border-radius: 10px;
+        font-weight: 500;
+
+        background-color: ${({ theme }) => theme.COLORS.PINK};
+        color: ${({ theme }) => theme.COLORS.BACKGROUND_950};
+
+        &:disabled {
+          opacity: 0.5;
+        }
+      }
+    }
+
+    svg {
+        color: ${({ theme }) => theme.COLORS.PINK};
+    }
+
+    .author {
+      margin-top: 24px;
+      margin-bottom: 40px;
+
+      display: flex;
+      align-items: center;
+      gap: 10px;
+
+      img {
+        width: 16px;
+        height: 16px;
+
+        border-radius: 35px;
+      }
+
+      #p {
+        color: ${({ theme }) => theme.COLORS.WHITE};
+      }
+    }
+  }
+`
+
+export const Content = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    font-size: 36px;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 50px;
+    text-align: justify;
+  }
+`
