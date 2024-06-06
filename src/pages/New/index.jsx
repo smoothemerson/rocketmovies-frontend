e1b398184944017258e490456595a38d9409d6cc
emerson <emerson8096@gmail.com>
2024-06-06 16:28:10 -0300

    add New

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+import { Container, Form } from "./styles";
+
+import { IoMdArrowBack } from "react-icons/io";
+
+import { api } from "../../services/api";
+
+export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [rating, setRating] = useState([]);
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  function handleAddTag() {
+    setTags((prevState) => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+  }
+
+  async function handleNewNote() {
+    if (!title) {
+      return alert("Digite o título do filme");
+    }
+
+    if (!rating) {
+      return alert(
+        "Você deixou uma nota no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio."
+      );
+    }
+
+    if (newTag) {
+      return alert(
+        "Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio."
+      );
+    }
+
+    await api.post("/movie_notes", {
+      title,
+      description,
+      tags,
+      rating,
+    });
+
+    alert("Nota de filme criado com sucesso!");
+    navigate(-1);
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/movie_notes/${params.id}`);
+      navigate(-1);
+    }
+  }
+
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <IoMdArrowBack onClick={handleBack} />
+            <ButtonText title="voltar" onClick={handleBack} />
+          </header>
+
+          <h1>Novo filme</h1>
+
+          <div id="movies">
+            <Input
+              placeholder="Título"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <Input
+              placeholder="Sua nota (de 0 a 5)"
+              onChange={(e) => setRating(e.target.value)}
+            />
+          </div>
+
+          <Textarea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <Section title="Marcadores">
+            <div className="tags">
+              {tags.map((tag, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
+              ))}
+              <NoteItem
+                $isNew
+                placeholder="Nova tag"
+                onChange={(e) => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
+            </div>
+          </Section>
+
+          <div className="buttons">
+            <Button exclude title="Excluir filme" onClick={handleRemove} />
+            <Button title="Salvar" onClick={handleNewNote} />
+          </div>
+        </Form>
+      </main>
+    </Container>
+  );
+}
---
+++ src/pages/New/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas: 
+  "header"
+  "content";
+
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+  }
+
+  .tags {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+`
+
+export const Form = styled.form`
+  max-width: 1100px;
+  margin: 38px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    margin-bottom: 36px;
+
+    color: ${({ theme }) => theme.COLORS.PINK};
+
+    a {
+      font-size: 20px;
+      color: ${({ theme }) => theme.COLORS.PINK};
+      gap: 10px;
+    }
+  }
+
+  #movies {
+    margin: 40px 0;
+
+    display: flex;
+    justify-content: space-between;
+    gap: 56px;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
+  }
+`
