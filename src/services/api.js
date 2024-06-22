e5a58cfd899145f9887d51556f678fb7565d5024
emerson <emerson8096@gmail.com>
2024-06-22 17:58:19 -0300

    fix: update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://rocketnotes-api-wenv.onrender.com"
+  baseURL: "https://rocketmovies-backend-np2g.onrender.com"
 })
