6ca562d8512fc722a3052a3e95a748d50a41d44d
emerson <emerson8096@gmail.com>
2024-07-09 07:36:10 -0300

    style: add PasswordCriteria and StatusCard

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -52,3 +52,55 @@ export const Background = styled.div`
   background: url(${backgroundImg}) no-repeat center center;
   background-size: cover;
 `
+
+export const PasswordCriteria = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  p {
+    font-weight: bold;
+    margin-block: 10px;
+    text-align: center;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    
+    li {
+      margin-bottom: 5px;
+    }
+  }
+`
+
+export const StatusCard = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px 40px;
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_950};
+  color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+  text-align: center;
+  font-size: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    margin-bottom: 20px;
+  }
+
+  > button {
+    background: ${({ theme }) => theme.COLORS.PINK};
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 8px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+`
